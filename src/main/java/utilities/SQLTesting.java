b6807f3c4980a745b5a955966a3be4565016d6d6
vinayak <vinayak@vinayak-Lenovo-Z51-70>
2022-12-19 21:01:21 +0530

    hh

--- src/main/java/utilities/SQLTesting.java
+++ src/main/java/utilities/SQLTesting.java
@@ -11,24 +11,21 @@ import com.mysql.cj.jdbc.result.ResultSetMetaData;
 
 public class SQLTesting {
 	UserCredentials userData = new UserCredentials();
-	
+	HashMap<String, String> map = new HashMap<>();
 
 	public static void main(String[] args) {
-		SQLTesting sql= new SQLTesting();
+		SQLTesting sql = new SQLTesting();
 		sql.Getdata();
 
 	}
 
 	public UserCredentials Getdata() {
-		//String Table, String TestCase
+		// String Table, String TestCase
 		try {
 			// Connection URL Syntax: "jdbc:mysql://ipaddress:portnumber/db_name"
 			String dbUrl = "jdbc:mysql://192.168.0.105:3306/helpdesk";
-			// Database Username
 			String username = "vinni";
-			// Database Password
 			String password = "D@vteam12";
-			// Query to Execute
 			String query = "select * from Login;";
 //			String query = "select * from "+Table+" where testCase='"+TestCase+"';";
 			// Load mysql jdbc driver
@@ -40,29 +37,32 @@ public class SQLTesting {
 			Statement stmt = con.createStatement();
 			// Execute the SQL Query. Store results in ResultSet
 			ResultSet rs = stmt.executeQuery(query);
-			
+
 			ResultSetMetaData rsMetaData = (ResultSetMetaData) rs.getMetaData();
-		    System.out.println("List of column names in the current table: ");
+			System.out.println("List of column names in the current table: ");
 			// While Loop to iterate through all data and print results
-		    int count = rsMetaData.getColumnCount();
-		      for(int i = 1; i<=count; i++) {
-		         System.out.print(rsMetaData.getColumnName(i)+" ");
-//		    	  HashMap<String, String> map = new HashMap<>();
-//		    	  map.put(rsMetaData.getColumnName(i), rs.getString(i));
-//		         String value = rs.getString(i);
-		      }
-		      System.out.println("\n");
-			while (rs.next()) {
-				String value = rs.getString(1);
-				System.out.println(value);
+			int count = rsMetaData.getColumnCount();
+			for (int i = 1; i <= count; i++) {
+				System.out.print(rsMetaData.getColumnName(i) + " ");
+//				while (rs.next()) {
+//					int k = 1;
+//					while (k <= count) {
+//						map.put(rsMetaData.getColumnName(i), rs.getString(i));
+//					}
+//				}
 			}
+			System.out.println(map+"aa ");
+//			while (rs.next()) {
+//				String value = rs.getString(1);
+//				System.out.println(value);
+//			}
 			// closing DB Connection
 			con.close();
 		} catch (SQLException sqle) {
 			System.out.println("there was an issue updating");
-	        System.out.println("SQLException: " + sqle.getMessage());
-	        System.out.println("SQLState: " + sqle.getSQLState());
-	        System.out.println("VendorError: " + sqle.getErrorCode());
+			System.out.println("SQLException: " + sqle.getMessage());
+			System.out.println("SQLState: " + sqle.getSQLState());
+			System.out.println("VendorError: " + sqle.getErrorCode());
 		} catch (ClassNotFoundException cnfe) {
 			cnfe.printStackTrace();
 			System.out.println("Exception occured while connecting to DB");
