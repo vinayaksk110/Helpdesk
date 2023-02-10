373d7e285603f9da50629e5f1df113a8ae685d74
Vinayak <vinayak.kumbar@idrive.com>
2023-02-10 14:58:23 +0530

    otpreaderutility

---
+++ src/main/java/utilities/OTPReader.java
@@ -0,0 +1,30 @@
+package utilities;
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class OTPReader {
+	
+	public static void main(String args[]) { 
+		OTPReader reader = new OTPReader();
+		reader.otpReaderUtility();
+	} 
+	
+	public String otpReaderUtility() {
+		RestAssured.baseURI = "https://api.twilio.com/2010-04-01/Accounts/ACc3104b3eedcee501f784298f87045af4/Messages.json"; 
+		RequestSpecification request = RestAssured.given(); 
+		request.auth().basic("ACc3104b3eedcee501f784298f87045af4", "0b95e1dbe024d8db27eebb38e6358cb7"); 
+		Response response = request.get(); 
+ 
+		//System.out.println(response.asString()); 
+		JsonPath myPath = new JsonPath(response.asString()); 
+		String body = myPath.getString("messages[0].body"); 
+		System.out.println(body); 
+		String otp=body.substring(0, 6); 
+		System.out.println(otp); 
+		return otp;
+	}
+
+}
